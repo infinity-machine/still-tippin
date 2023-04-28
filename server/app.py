494d1eed80ae877b40e1668fd16e0c84a4467dcb
Connor Carciofini <ccarciofini@gmail.com>
2023-04-28 16:35:36 -0500

    cleanup

--- server/app.py
+++ server/app.py
@@ -7,11 +7,3 @@ def index():
     return flask.render_template('index.html')
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-# def create_app(testing: bool = True):
-#     app = flask(__name__)
-#     @app.route('/')
-#     def index():
-#         return flask.render_template('index.html')
-#     return app
--- server/main.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-app = flask.Flask('__main__')
-
-@app.route('/')
-def root():
-    return flask.render_template('index.html')
-
-app.run(debug=True)
